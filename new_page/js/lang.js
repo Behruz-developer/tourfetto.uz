6d64a9c3edd9de13d98c14a3a73a76ab71c4b2ab
Behruz-developer <88540036+Behruz-developer@users.noreply.github.com>
2023-12-24 00:56:38 +0500

    errors handling

---
+++ new_page/js/lang.js
@@ -0,0 +1,153 @@
+const langData = {
+    "uz": {
+      "home": "Bosh Sahifa",
+      "about": "Biz haqimizda",
+      "new": "Yangiliklar",
+      "tour": "Category",
+      "tour1": "Sayohat",
+      "contact": "Kontakt",
+      "intro_text1": "Biz bilan maroqli sayohat",
+      "intro_text2": "Bosh sahifa>",
+      "description_text1": "10 kun Tainlant da",
+      "description_text2": "tajribalarini yaratishga intiladi. Shu bois, biz sizga Ispaniya, Italiya va Gretsiyadagi yangi va ajoyib sayohat paketlarini taqdim etishdan mamnunmiz. Bu mo'jazgina tavsif sizga ushbu go'zal mamlakatlardagi sayohatlarimizning qanday bo'lishi haqida tasavvurga ega bo'lishingizda yordam beradi.",
+      "description_text3": "Ispaniya: Ispaniya, o'zining boy tarixi, rang-barang madaniyati va hayratlanarli me'morchiligi bilan mashhur. Bu yerda siz eski shaharlar, qadimgi qasrlar va zamonaviy san'at asarlarini topasiz. Barselona shahridagi Gaudi me'morchiligi va Madridning tarixiy markazlari kabi diqqatga sazovor joylar sizni kutmoqda. Shuningdek, flamenko raqslari va mazali ispan taomlari sizni hayratda qoldiradi.",
+      "news_text1": "Yangiliklar",
+      "": "",
+      "": "",
+      "footer_text1": "TOURFETTO sayyohlik agentligi xalqaro bozorda taqdim etilgan 2023 yildan turizm.",
+      "footer_text2": "Toshkent Shahar",
+      "footer_text3": "Menyu",
+      "footer_text4": "Bosh sahifa",
+      "footer_text5": "Biz haqimizda",
+      "footer_text6": "Yangiliklar",
+      "footer_text7": "Catigory",
+      "footer_text8": "Kontakt",
+      "footer_text9": "Mahhur shaharlar",
+      "footer_text10": "Tailand",
+      "footer_text11": "Daubai",
+      "footer_text12": "Turkiya",
+      "footer_text13": "Shvedsiya",
+      "footer_text14": "Gruziya",
+      "footer_text15": "Vetnam",
+      "footer_text16": "Biz ijtimoiy tarmoqlardamiz",
+      "": "",
+      "": "",
+  
+      
+    },
+    "ru": {
+      "home": "Главная страница",
+      "about": "о нас",
+      "new": "Новости",
+      "tour": "Категория",
+      "tour1": "Тур",
+      "contact": "Контакт",
+      "img_slider1": "с нами",
+      "img_slider2": "желаю хорошей поездки",
+      "intro_text1": "Наслаждайтесь поездкой вместе с нами",
+      "intro_text2": "Главная страница>>",
+      "description_text1": "10 дней в Таиланде",
+      "description_text2": "стремится создавать впечатления. Поэтому мы рады представить вам новые и удивительные туристические пакеты в Испанию, Италию и Грецию. Это краткое описание поможет вам составить представление о том, какими будут наши туры по этим прекрасным странам.",
+      "description_text3": "Испания: Испания известна своей богатой историей, красочной культурой и потрясающей архитектурой. Здесь вы найдете старые города, старинные замки и современные произведения искусства. Вас ждут такие достопримечательности, как архитектура Гауди в Барселоне и исторические центры Мадрида. Вы также будете поражены танцами фламенко и вкусной испанской едой.",
+      "news_text1": "Новости",
+      "": "",
+      "": "",
+      "footer_text1": "Туризм с 2023 года, туристическое агентство TOURFETTO представлено на международном рынке.",
+      "footer_text2": "город Ташкент",
+      "footer_text3": "Меню",
+      "footer_text4": "Главная страница",
+      "footer_text5": "о нас",
+      "footer_text6": "Новости",
+      "footer_text7": "Категория",
+      "footer_text8": "Контакт",
+      "footer_text9": "Знаменитые города",
+      "footer_text10": "Таиланд",
+      "footer_text11": "Дубай",
+      "footer_text12": "Турция",
+      "footer_text13": "Швеция",
+      "footer_text14": "Грузия",
+      "footer_text15": "Vetnam",
+      "footer_text16": "Мы в социальных сетях",
+      "": "", 
+      "": "", 
+      "": "", 
+      "": "", 
+      "": "", 
+      "": "", 
+  
+  
+  
+  
+    },
+    "en": {
+      "home": "Main page",
+      "about": "about Us",
+      "new": "News",
+      "tour": "Category",
+      "tour1": "Tour",
+      "contact": "Contact",
+      "img_slider1": "with us",
+      "img_slider2": "have a nice trip",
+      "intro_text1": "Enjoy your trip with us",
+      "intro_text2": "Homepage>",
+      "description_text1": "10 days in Thailand",
+      "description_text2": "seeks to create experiences. Therefore, we are pleased to present you with new and amazing travel packages in Spain, Italy and Greece. This brief description will help you get an idea of ​​what our tours in these beautiful countries will be like.",
+      "description_text3": "Spain: Spain is known for its rich history, colorful culture and stunning architecture. Here you will find old towns, ancient castles and modern works of art. Sights such as Barcelona's Gaudi architecture and Madrid's historic centers await you. You will also be amazed by flamenco dances and delicious Spanish food.",
+      "news_text1": "News",
+      "": "",
+      "": "",
+      "footer_text1": "Tourism since 2023, TOURFETTO travel agency presented on the international market.",
+      "footer_text2": "Tashkent city",
+      "footer_text3": "Menu",
+      "footer_text4": "Homepage",
+      "footer_text5": "about Us",
+      "footer_text6": "News",
+      "footer_text7": "Category",
+      "footer_text8": "Contact",
+      "footer_text9": "Famous cities",
+      "footer_text10": "Thailand",
+      "footer_text11": "Dubai",
+      "footer_text12": "Turkey",
+      "footer_text13": "Sweden",
+      "footer_text14": "Georgia",
+      "footer_text15": "Vietnam",
+      "footer_text16": "We are on social media",
+      "": "",
+      "": "",
+      "": "",
+      "": "",
+      "": "",
+      "": "",
+      "": "",
+      "": "",
+      "": "",  
+    }
+  };
+  
+  const langElements = document.querySelectorAll('.lang');
+  
+  function changeLanguage(language) {
+    selectedLanguage = language;
+    langElements.forEach(el => {
+      el.textContent = langData[selectedLanguage][el.getAttribute('key')];
+    });
+  }
+  
+  
+  
+  // Tilni qayta belgilash
+  let selectedLanguage = 'ru';
+  langElements.forEach(el => {
+    el.textContent = langData[selectedLanguage][el.getAttribute('key')];
+  });
+  
+  const langButtons = document.querySelectorAll('.lang-button');
+  langButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    changeLanguage(btn.dataset.lang);
+    langButtons.forEach(btn => {
+      btn.classList.remove('active_lang');
+    });
+    btn.classList.add('active_lang');
+  });
+  });
---
+++ tour_page/js/lang.js
@@ -0,0 +1,147 @@
+const langData = {
+    "uz": {
+      "home": "Bosh Sahifa",
+      "about": "Biz haqimizda",
+      "new": "Yangiliklar",
+      "tour": "Category",
+      "tour1": "Sayohat",
+
+      "contact": "Kontakt",
+      "about_text1": "Gruziya",
+      "description_text1": "Tavsif",
+      "description_text2": "Bu kabi sayohatlar sayyohlarga dunyoning turli burchaklarida yashayotgan xalqlarning madaniyati va turmush tarzini chuqurroq tushunish imkoniyatini beradi. Bunday tajribalar odamlarni boshqa mamlakatlarning boy tarixi va rang-barang madaniyatlariga yaqindan guvoh bo'lishga olib keladi. Sayohatchilar, odatda, tarixiy qadriyatlarni, jumladan qadimgi obidalar, muzeylar va san'at galereyalari kabi diqqatga sazovor joylarni ziyorat qilishadi. Bu yerlarda ular mahalliy san'at asarlari va tarixiy yodgorliklarni ko'rib, ularning tarixi va ahamiyati haqida ma'lumot oladilar.",
+      "description_text3": "Sayohatlar, shubhasiz, odamlarga dunyoni ko'rish va yangi madaniyatlarni tushunish imkoniyatini beradi. Sayohat paytida inson yangi joylarni kashf etadi, turli xil madaniyatlar va urf-odatlarni o'rganadi, bu esa uning dunyoqarashini boyitadi. Sayohatlar, shuningdek, yangi do'stlar orttirish va turli insonlar bilan muloqot qilish imkoniyatini yaratadi, bu esa ijtimoiy tarmoqlarni kengaytiradi va odamlararo munosabatlarni mustahkamlaydi. Bunday sarguzashtlar har bir kishining hayot tajribasini oshiradi, yangi ko'nikma va bilimlarni o'rgatadi.",
+      "": "",
+      "footer_text1": "TOURFETTO sayyohlik agentligi xalqaro bozorda taqdim etilgan 2023 yildan turizm.",
+      "footer_text2": "Toshkent Shahar",
+      "footer_text3": "Menyu",
+      "footer_text4": "Bosh sahifa",
+      "footer_text5": "Biz haqimizda",
+      "footer_text6": "Yangiliklar",
+      "footer_text7": "Catigory",
+      "footer_text8": "Kontakt",
+      "footer_text9": "Mahhur shaharlar",
+      "footer_text10": "Tailand",
+      "footer_text11": "Daubai",
+      "footer_text12": "Turkiya",
+      "footer_text13": "Shvedsiya",
+      "footer_text14": "Gruziya",
+      "footer_text15": "Vetnam",
+      "footer_text16": "Biz ijtimoiy tarmoqlardamiz",
+      "": "",
+      "": "",
+  
+      
+    },
+    "ru": {
+      "home": "Главная страница",
+      "about": "о нас",
+      "new": "Новости",
+      "tour": "Категория",
+      "tour1": "Тур",
+
+      "contact": "Контакт",
+      "img_slider1": "с нами",
+      "img_slider2": "желаю хорошей поездки",
+      "about_text1": "Грузия",
+      "description_text1": "Описание",
+      "description_text2": "Такие поездки дают туристам возможность глубже понять культуру и образ жизни людей, живущих в разных уголках мира. Такой опыт позволяет людям стать свидетелями богатой истории и разнообразия культур других стран. Туристы обычно посещают места, представляющие историческую ценность, в том числе древние памятники, музеи и художественные галереи. Здесь они видят местные произведения искусства и исторические памятники, узнают об их истории и значении.",
+      "description_text3": "Путешествия, несомненно, дают людям возможность увидеть мир и понять новые культуры. Путешествуя, человек открывает для себя новые места, познает разные культуры и традиции, что обогащает его мировоззрение. Путешествие также дает возможность завести новых друзей и пообщаться с разными людьми, что расширяет социальные сети и укрепляет межличностные отношения. Такие приключения приумножают жизненный опыт каждого, учат новым навыкам и знаниям.",
+
+      "footer_text1": "Туризм с 2023 года, туристическое агентство TOURFETTO представлено на международном рынке.",
+      "footer_text2": "город Ташкент",
+      "footer_text3": "Меню",
+      "footer_text4": "Главная страница",
+      "footer_text5": "о нас",
+      "footer_text6": "Новости",
+      "footer_text7": "Категория",
+      "footer_text8": "Контакт",
+      "footer_text9": "Знаменитые города",
+      "footer_text10": "Таиланд",
+      "footer_text11": "Дубай",
+      "footer_text12": "Турция",
+      "footer_text13": "Швеция",
+      "footer_text14": "Грузия",
+      "footer_text15": "Vetnam",
+      "footer_text16": "Мы в социальных сетях",
+      "": "", 
+      "": "", 
+      "": "", 
+      "": "", 
+      "": "", 
+      "": "", 
+  
+  
+  
+  
+    },
+    "en": {
+      "home": "Main page",
+      "about": "about Us",
+      "new": "News",
+      "tour": "Category",
+      "tour1": "Tour",
+
+      "contact": "Contact",
+      "img_slider1": "with us",
+      "img_slider2": "have a nice trip",
+      "about_text1": "Georgia",
+      "description_text1": "Description",
+      "description_text2": "Such trips give tourists the opportunity to gain a deeper understanding of the culture and lifestyle of people living in different parts of the world. Such experiences lead people to witness the rich history and diverse cultures of other countries up close. Tourists usually visit places of historical value, including ancient monuments, museums and art galleries. Here they see local artworks and historical monuments and learn about their history and significance.",
+      "description_text3": "Travel undoubtedly gives people the opportunity to see the world and understand new cultures. While traveling, a person discovers new places, learns different cultures and traditions, which enriches his worldview. Traveling also provides an opportunity to make new friends and interact with different people, which expands social networks and strengthens interpersonal relationships. Such adventures increase everyone's life experience, teach new skills and knowledge.",
+
+      "footer_text1": "Tourism since 2023, TOURFETTO travel agency presented on the international market.",
+      "footer_text2": "Tashkent city",
+      "footer_text3": "Menu",
+      "footer_text4": "Homepage",
+      "footer_text5": "about Us",
+      "footer_text6": "News",
+      "footer_text7": "Category",
+      "footer_text8": "Contact",
+      "footer_text9": "Famous cities",
+      "footer_text10": "Thailand",
+      "footer_text11": "Dubai",
+      "footer_text12": "Turkey",
+      "footer_text13": "Sweden",
+      "footer_text14": "Georgia",
+      "footer_text15": "Vietnam",
+      "footer_text16": "We are on social media",
+      "": "",
+      "": "",
+      "": "",
+      "": "",
+      "": "",
+      "": "",
+      "": "",
+      "": "",
+      "": "",  
+    }
+  };
+  
+  const langElements = document.querySelectorAll('.lang');
+  
+  function changeLanguage(language) {
+    selectedLanguage = language;
+    langElements.forEach(el => {
+      el.textContent = langData[selectedLanguage][el.getAttribute('key')];
+    });
+  }
+  
+  
+  
+  // Tilni qayta belgilash
+  let selectedLanguage = 'ru';
+  langElements.forEach(el => {
+    el.textContent = langData[selectedLanguage][el.getAttribute('key')];
+  });
+  
+  const langButtons = document.querySelectorAll('.lang-button');
+  langButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    changeLanguage(btn.dataset.lang);
+    langButtons.forEach(btn => {
+      btn.classList.remove('active_lang');
+    });
+    btn.classList.add('active_lang');
+  });
+  });
