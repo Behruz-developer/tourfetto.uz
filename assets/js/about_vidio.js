4060e3616f083c73f7e687a18e74d76d63836d01
Behruz-developer <88540036+Behruz-developer@users.noreply.github.com>
2023-12-26 11:05:26 +0500

    finish project

--- assets/js/about_vidio.js
+++ assets/js/about_vidio.js
@@ -1,25 +1,66 @@
-function openNav(navId, videoId) {
-    document.getElementById(navId).style.display = "block";
-    document.getElementById(videoId).play();
-  }
+// function openNav(navId, videoId) {
+//     document.getElementById(navId).style.display = "block";
+//     document.getElementById(videoId).play();
+//   }
   
-  function closeNav(navId, videoId) {
-    document.getElementById(navId).style.display = "none";
-    document.getElementById(videoId).pause();
-  }
-  document.addEventListener("DOMContentLoaded", function() {
-    var videos = document.querySelectorAll('video');
+//   function closeNav(navId, videoId) {
+//     document.getElementById(navId).style.display = "none";
+//     document.getElementById(videoId).pause();
+//   }
+//   document.addEventListener("DOMContentLoaded", function() {
+//     var videos = document.querySelectorAll('video');
   
-    videos.forEach(function(video) {
-        video.addEventListener('play', function() {
-            var sources = video.querySelectorAll('source[data-src]');
-            sources.forEach(function(source) {
-                source.setAttribute('src', source.getAttribute('data-src'));
-                source.removeAttribute('data-src');
-            });
-            if (sources.length > 0) {
-                video.load();
-            }
-        });
-    });
+//     videos.forEach(function(video) {
+//         video.addEventListener('play', function() {
+//             var sources = video.querySelectorAll('source[data-src]');
+//             sources.forEach(function(source) {
+//                 source.setAttribute('src', source.getAttribute('data-src'));
+//                 source.removeAttribute('data-src');
+//             });
+//             if (sources.length > 0) {
+//                 video.load();
+//             }
+//         });
+//     });
+//   });
+
+
+
+function openNav(navId) {
+  var modal = document.getElementById(navId);
+  modal.style.display = "block";
+
+  // Modal ichidagi video topib, uni ijro etish
+  var video = modal.querySelector('video');
+  if (video) {
+      video.play();
+  }
+}
+
+function closeNav(navId) {
+  var modal = document.getElementById(navId);
+  modal.style.display = "none";
+
+  // Modal ichidagi video topib, uni to'xtatish
+  var video = modal.querySelector('video');
+  if (video) {
+      video.pause();
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  var videos = document.querySelectorAll('video');
+
+  videos.forEach(function(video) {
+      video.addEventListener('play', function() {
+          var sources = video.querySelectorAll('source[data-src]');
+          sources.forEach(function(source) {
+              source.setAttribute('src', source.getAttribute('data-src'));
+              source.removeAttribute('data-src');
+          });
+          if (sources.length > 0) {
+              video.load();
+          }
+      });
   });
+});
